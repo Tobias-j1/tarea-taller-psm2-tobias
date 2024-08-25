773f149791a271a30812f507c527d0536740ccd6
Tobias'j1 <tobiasismaeljara@fpuna.edu.py>
2024-08-25 16:34:50 -0400

    Se agrega funcion promedio

--- Hola_mundo.c
+++ Hola_mundo.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
+int a;
+int b;
+
 int main(void){
 
     printf("Hola mundo");
     printf("como studio --> stdio");
+
+    printf("Ingrese  1er valor: ");
+    scanf("%d",&a);
+
+    printf("Ingrese  2do valor: ");
+    scanf("%d",&b);
+
+    printf("El promedio es: %0.3f",(float)((a+b)/2));
 
     return(0);
 
